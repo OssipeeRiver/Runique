c5bcf58d34cb5c552d3f206ce8fb56f30260ae0d
unknown <ossipeerivermediagroup@outlook.com>
2024-05-02 13:01:14 -0400

    completed 4.6 setting up the build logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,11 +1,11 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+//pluginManagement {
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//}
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
