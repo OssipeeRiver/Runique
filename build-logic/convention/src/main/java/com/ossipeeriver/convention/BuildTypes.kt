b2286ce0aecce4db0b1bbd31e490e1e47fc9bdcd
unknown <ossipeerivermediagroup@outlook.com>
2024-05-04 15:24:12 -0400

    began setting up build types 4.7.2

---
+++ build-logic/convention/src/main/java/com/ossipeeriver/convention/BuildTypes.kt
@@ -0,0 +1,14 @@
+package com.ossipeeriver.convention
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.external.cmake.server.Project
+
+
+internal fun Project.configureBuildTypes(
+    commonExtension: CommonExtension<*, *, *, * , *>,
+
+) {
+    commonExtension.run {
+
+    }
+}
---
+++ build-logic/convention/src/main/java/com/ossipeeriver/convention/ExtensionType.kt
@@ -0,0 +1,6 @@
+package com.ossipeeriver.convention
+
+enum class ExtensionType {
+    APPLICATION,
+    LIBRARY
+}
