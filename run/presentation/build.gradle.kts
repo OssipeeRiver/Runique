137710af4bd28c4b7229a84af0dbc93c94fb40d9
Anthony Smith <ossipeerivermediagroup@outlook.com>
2024-05-09 23:32:21 -0400

    successful build completed AndroidFeatureUiConventionPlugin

--- run/presentation/build.gradle.kts
+++ run/presentation/build.gradle.kts
@@ -1,35 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+//    alias(libs.plugins.android.library)
+//    alias(libs.plugins.jetbrainsKotlinAndroid)
+
+    alias(libs.plugins.runique.android.feature.ui)
 }
 
 android {
     namespace = "com.ossipeeriver.run.presentation"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
@@ -38,6 +16,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.timber)
 
-//    implementation(projects.core.domain)
-//    implementation(projects.run.domain)
+    implementation(projects.core.domain)
+    implementation(projects.run.domain)
 }
