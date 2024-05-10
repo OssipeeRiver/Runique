d70e451608529c113ccecd7eefd61f312cb80d81
Anthony Smith <ossipeerivermediagroup@outlook.com>
2024-05-09 23:15:56 -0400

    successful build after AndroidFeatureUiConventionPlugin

---
+++ build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
@@ -0,0 +1,22 @@
+import com.android.build.gradle.LibraryExtension
+import com.ossipeeriver.convention.addUiLayerDependencies
+import com.ossipeeriver.convention.configureAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+
+class AndroidFeatureUiConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("runique.android.library.compose")
+            }
+
+            dependencies {
+                addUiLayerDependencies(target)
+            }
+        }
+    }
+}
